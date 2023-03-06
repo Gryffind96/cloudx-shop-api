a8633550856d9f59f7c57997215bb52741132aff
Erwin  Bustillo <erwinb1996@gmail.com>
2023-03-06 09:32:27 -0500

    Task 4 (#2)
    * integrating product dynamo db
    
    * fixed serverless deployment, added schema validation on POST product
    
    * more fixes
    
    * product output on creation
    
    * added stock support for list products
    
    * fixed localhost debug
    
    * fixed small bug
    
    * whitelisted new cloudfront distribution
    
    * added logs to lambda events

--- getProductById.js
+++ getProductById.js
@@ -1,22 +1,28 @@
 'use strict';
 
-const { products } = require("./products");
-
-module.exports.handler = async (event) => {
+const dynamoInstance = require('./db')
+module.exports.handler = async (event,context) => {
+  console.info("EVENT\n" + JSON.stringify(event, null, 2))
+  console.log(context.logStreamName)
   const { productId } = event.pathParameters;
   try {
-    const product = products.find(product => product.id === productId)   
-    if (!product) {
-      throw Error('Product not found!')
+    const product = await dynamoInstance.get({
+      TableName: process.env.PRODUCTS_TABLE,
+      Key: {
+        id: productId
+      }
+    }).promise();
+    if (!product.Item) {
+      throw new Error('Product not found')
     }
     return {
       statusCode: 200,
-      body: JSON.stringify(product)
+      body: JSON.stringify(product.Item)
     };
   } catch (error) {
     return {
       statusCode: 404,
-      body: JSON.stringify({ message: 'Product not found' })
+      body: JSON.stringify({ message: error.message })
     };
   }
 };
--- getProducts.js
+++ getProducts.js
@@ -1,10 +1,33 @@
 'use strict';
 
-const { products } = require("./products");
-
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(products),
-  };
+const dynamoInstance = require('./db')
+module.exports.handler = async (event,context) => {
+  console.info("EVENT\n" + JSON.stringify(event, null, 2))
+  console.log(context.logStreamName)
+  try {
+    const products = await dynamoInstance.scan({ TableName: process.env.PRODUCTS_TABLE }).promise();
+    const productsResponse = [];
+    for (let i = 0; i < products.Items.length; i++) {
+      let stock = await dynamoInstance.get({ 
+        TableName: process.env.STOCKS_TABLE,
+        Key: {
+          product_id: products.Items[i].id 
+        }
+      }).promise();
+    
+      productsResponse.push({
+        ...products.Items[i],
+        count: stock.Item?.count || 0
+      })
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(productsResponse) 
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error
+    };
+  }
 };
