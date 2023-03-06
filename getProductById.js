d5f4b7ad936e24a0306b31186e05ac883107df5b
Erwin Bustillo <erwinb1996@outlook.com>
2023-03-05 22:49:27 -0500

    more fixes

--- getProductById.js
+++ getProductById.js
@@ -4,15 +4,23 @@ const dynamoInstance = require('./db')
 module.exports.handler = async (event) => {
   const { productId } = event.pathParameters;
   try {
-    const product = await dynamoInstance.get({ 
+    const product = await dynamoInstance.get({
       TableName: process.env.PRODUCTS_TABLE,
       Key: {
         id: productId
       }
     }).promise();
-    if (!product.Item) throw Error('Product not found');
-    return product.Item;
+    if (!product.Item) {
+      throw new Error('Product not found')
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(product.Item)
+    };
   } catch (error) {
-    throw Error(error)
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: error.message })
+    };
   }
 };
