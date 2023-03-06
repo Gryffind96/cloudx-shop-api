c16ecbf097ed39c7d486179f21f8297f5f43e096
Erwin Bustillo <erwinb1996@outlook.com>
2023-03-05 23:34:01 -0500

    added stock support for list products

--- getProducts.js
+++ getProducts.js
@@ -2,6 +2,30 @@
 
 const dynamoInstance = require('./db')
 module.exports.handler = async (event) => {
-  const response = await dynamoInstance.scan({ TableName: process.env.PRODUCTS_TABLE }).promise();
-  return response.Items;
+  try {
+    const products = await dynamoInstance.scan({ TableName: process.env.PRODUCTS_TABLE }).promise();
+    const productsResponse = [];
+    for (let i = 0; i < products.Items.length; i++) {
+      let stock = await dynamoInstance.get({ 
+        TableName: process.env.STOCKS_TABLE,
+        Key: {
+          product_id: products.Items[i].id 
+        }
+      }).promise();
+    
+      productsResponse.push({
+        ...products.Items[i],
+        count: stock.Item?.count || 0
+      })
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(productsResponse) 
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error
+    };
+  }
 };
