03e1daad08fa7001456cda83266579585e486f46
Erwin Bustillo <erwinb1996@outlook.com>
2023-03-15 09:15:40 -0500

    fixes

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -5,6 +5,7 @@ const {
   GetObjectCommand,
   S3Client,
 } = require('@aws-sdk/client-s3')
+const { Readable } = require('stream')
 const client = new S3Client({ region: 'us-east-1' })
 module.exports.handler = async (event) => {
   try {
