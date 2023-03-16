7de3cb90078901655efe482f23d569606a75f5b4
Erwin Bustillo <erwinb1996@outlook.com>
2023-03-16 10:53:53 -0500

    mend

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -17,7 +17,7 @@ module.exports.handler = async (event) => {
       throw new Error();
     }
 
-    const asyncRecords = records.map(async (record, index) => {
+    records.map(async (record, index) => {
       const RECORD_INFO = `[RECORD ${index + 1} of ${recordsLength}]`;
       const params = {
         Bucket: process.env.BUCKET_NAME,
@@ -30,48 +30,46 @@ module.exports.handler = async (event) => {
         )
       ).Body;
 
-      if (!(streamBody instanceof Readable)) {
-        throw new Error(`${RECORD_INFO} is not readable stream`);
-      }
-      return new Promise((resolve, reject) => {
-        streamBody
-          .pipe(csv())
-          .on('data', (data) => {
-            console.log(RECORD_INFO, `Parsing product import CSV data: `, data);
-          })
-          .on('error', (error) => {
-            console.error(RECORD_INFO, `Parsing error for product import CSV data: `, error);
-            reject(new Error(`${RECORD_INFO} error processing the file`));
-          })
-          .on('end', async () => {
-            console.log(
-              RECORD_INFO,
-              `Product data parsed. Moving to "/parsed" folder has started.`
-            );
-            await client.send(
-              new CopyObjectCommand({
-                Bucket: process.env.BUCKET_NAME,
-                CopySource: `${process.env.BUCKET_NAME}/${record.s3.object.key}`,
-                Key: record.s3.object.key.replace('uploaded', 'parsed'),
-              }),
-            )
-            console.log('copied successfully')
+      // if (!(streamBody instanceof Readable)) {
+      //   throw new Error(`${RECORD_INFO} is not readable stream`);
+      // }
 
-            await client.send(
-              new DeleteObjectCommand({
-                Bucket: process.env.BUCKET_NAME,
-                Key: record.s3.object.key,
-              }),
-            )
-            console.log('deleted successfully')
+      streamBody
+        .pipe(csv())
+        .on('data', (data) => {
+          console.log(RECORD_INFO, `Parsing product import CSV data: `, data);
+        })
+        .on('error', (error) => {
+          console.error(RECORD_INFO, `Parsing error for product import CSV data: `, error);
+          throw new Error(`${RECORD_INFO} error processing the file`);
+        })
+        .on('end', async () => {
+          console.log(
+            RECORD_INFO,
+            `Product data parsed. Moving to "/parsed" folder has started.`
+          );
+          await client.send(
+            new CopyObjectCommand({
+              Bucket: process.env.BUCKET_NAME,
+              CopySource: `${process.env.BUCKET_NAME}/${record.s3.object.key}`,
+              Key: record.s3.object.key.replace('uploaded', 'parsed'),
+            }),
+          )
+          console.log('copied successfully')
 
-            console.log(RECORD_INFO, 'Uploaded record deleted successfully!');
-            resolve('ok')
-          });
-      })
+          await client.send(
+            new DeleteObjectCommand({
+              Bucket: process.env.BUCKET_NAME,
+              Key: record.s3.object.key,
+            }),
+          )
+          console.log('deleted successfully')
+
+          console.log(RECORD_INFO, 'Uploaded record deleted successfully!');
+
+        });
+
     })
-
-    await Promise.all(asyncRecords);
 
     return {
       statusCode: 202,
