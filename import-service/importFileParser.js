de8df1106197014208b85739080bc40029c34ab9
Erwin Bustillo <erwinb1996@outlook.com>
2023-03-16 12:09:26 -0500

    more changes

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -1,82 +1,53 @@
-const {
-  CopyObjectCommand,
-  DeleteObjectCommand,
-  GetObjectCommand,
-  S3Client,
-} = require('@aws-sdk/client-s3')
-const { Readable } = require('stream')
-const client = new S3Client({ region: 'us-east-1' })
-const csv = require('csv-parser')
+let csvParser = require('csv-parser')
+const AWS = require('aws-sdk')
 module.exports.handler = async (event) => {
   try {
+    const s3 = new AWS.S3({ region: 'us-east-1' })
     const records = event.Records;
     const recordsLength = records.length;
     console.log('Import file parser lambda was triggered with records: ', records);
-
+    const BUCKET = process.env.BUCKET_NAME
     if (recordsLength === 0) {
+      console.log('No records to process')
       throw new Error();
     }
-
-    records.map(async (record, index) => {
-      const RECORD_INFO = `[RECORD ${index + 1} of ${recordsLength}]`;
+    console.log("Bucket", BUCKET)
+    for (const record of records) {
+      console.log(`Processing Record: ${record.s3.object.key}...`)
       const params = {
-        Bucket: process.env.BUCKET_NAME,
+        Bucket: BUCKET,
         Key: record.s3.object.key,
       }
 
-      const streamBody = (
-        await s3Client.send(
-          new GetObjectCommand(params)
-        )
-      ).Body;
+      const s3Stream = s3
+        .getObject(params)
+        .createReadStream()
 
-      // if (!(streamBody instanceof Readable)) {
-      //   throw new Error(`${RECORD_INFO} is not readable stream`);
-      // }
-
-      streamBody
-        .pipe(csv())
+      s3Stream
+        .pipe(csvParser())
         .on('data', (data) => {
-          console.log(RECORD_INFO, `Parsing product import CSV data: `, data);
-        })
-        .on('error', (error) => {
-          console.error(RECORD_INFO, `Parsing error for product import CSV data: `, error);
-          throw new Error(`${RECORD_INFO} error processing the file`);
+          console.log(data)
         })
         .on('end', async () => {
+          console.log(`Copy from ${BUCKET}/${record.s3.object.key}`)
+          await s3
+            .copyObject({
+              Bucket: BUCKET,
+              CopySource: `${BUCKET}/${record.s3.object.key}`,
+              Key: record.s3.object.key.replace('uploaded', 'parsed')
+            })
+            .promise()
           console.log(
-            RECORD_INFO,
-            `Product data parsed. Moving to "/parsed" folder has started.`
-          );
-          await client.send(
-            new CopyObjectCommand({
-              Bucket: process.env.BUCKET_NAME,
-              CopySource: `${process.env.BUCKET_NAME}/${record.s3.object.key}`,
-              Key: record.s3.object.key.replace('uploaded', 'parsed'),
-            }),
+            `Copied into ${BUCKET}/${record.s3.object.key.replace(
+              'uploaded',
+              'parsed'
+            )}`
           )
-          console.log('copied successfully')
 
-          await client.send(
-            new DeleteObjectCommand({
-              Bucket: process.env.BUCKET_NAME,
-              Key: record.s3.object.key,
-            }),
-          )
-          console.log('deleted successfully')
-
-          console.log(RECORD_INFO, 'Uploaded record deleted successfully!');
-
-        });
-
-    })
-
-    return {
-      statusCode: 202,
-      body: JSON.stringify({
-        message: 'File parsed!',
-      }),
+          await s3.deleteObject(params)
+        })
     }
+
   } catch (error) {
     return {
       statusCode: 500,
